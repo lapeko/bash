0fad57c76c5e05bab851b42eb87506a127a8531c
lapeko <lapeko88@gmail.com>
2025-06-13 08:47:32 +0200

    implement through {2..20}

--- free_exercises/for/3.sh
+++ free_exercises/for/3.sh
@@ -2,6 +2,14 @@
 
 # Print only even numbers from 2 to 20.
 
-for i in $(seq 2 2 20); do
-  echo $i
+# advanced implementation
+# for i in $(seq 2 2 20); do
+#   echo $i
+# done
+
+# simple implementation
+for i in {2..20}; do
+  if [[ $((i % 2)) == 0 ]]; then
+    echo $i
+  fi
 done
